3b0cca197c1f8b475b2333229716182d12bca9c4
vamshirao12 <vamshirao1207@gmail.com>
2026-04-19 14:49:57 +0530

    feat: add GET /api/v1/posts/:postId endpoint

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -6,4 +6,7 @@ const postController = require('../controllers/posts.controller.js');
 // 2. Point the route to the controller function
 router.get('/posts', postController.getAllPosts);
 
+// This handles GET /api/v1/posts/:postId
+router.get('/posts/:postId', postController.getPostById);
+
 module.exports = router;
